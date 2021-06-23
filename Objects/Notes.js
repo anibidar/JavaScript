3bd60171625ddb287da63329958b23de5490e6b1
Ani Bidar <atbidar@outlook.com>
2021-06-23 10:58:12 -0500

    New Upload
    Upload

---
+++ Objects/Notes.js
@@ -0,0 +1,18 @@
+/** Object Containers
+* Objects are typically Constants
+* We can change the properties of the object inside the container 
+* We can't remove or replace the object from the container
+*/
+
+/** Object Properties
+ * Describe different aspect of the object
+ * Syntax: Key: "Value",
+ * Property names can only consists of letters, numbers, dollar signs, and underscores. Any others cause JS to crash
+ * use camelCase
+ */
+
+/** Accessing Objects
+ * To access, call the name
+ */ 
+
+    
